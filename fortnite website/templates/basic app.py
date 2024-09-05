05a90dfb54ca05ccbb9be2fe4570b908a6da9d20
Geno Seuala <gs22296@my.westlake.school.nz>
2024-09-05 12:48:11 +1200

    comments

--- fortnite website/templates/basic app.py
+++ fortnite website/templates/basic app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Route to add a game
 @app.route("/register")
 def add_game():
     return render_template('register.html')
